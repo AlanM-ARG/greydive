e45f0ac7ebf77c5b8fbc55730805531c958b98a8
Alan <alanmorua8@gmail.com>
2023-01-11 03:45:40 -0300

    initial

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,15 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCKP2hk12beDuvRw_f7ZSdQCAehu78S67E",
-  authDomain: "greydive-f9484.firebaseapp.com",
-  projectId: "greydive-f9484",
-  storageBucket: "greydive-f9484.appspot.com",
-  messagingSenderId: "94387350538",
-  appId: "1:94387350538:web:4aeb935e491c4388ec03f3",
-  measurementId: "G-E1FWYX09D7"
-};
+const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG
 
 // Initialize Firebase
 const greydive = initializeApp(firebaseConfig);
